de2d5adc2358fb085b7c260cc4ae546157e95bb8
CKjaer <ChristianKjaerNielsen@gmail.com>
2025-05-14 12:37:28 +0200

    Moved UKF to library files

---
+++ deadReckoning/UKF.h
@@ -0,0 +1,77 @@
+#pragma once
+#include <cmath>
+#include <BasicLinearAlgebra.h>
+#include <array>
+#include <cstdint>
+
+class UKF
+{
+public:
+    static constexpr int n_x{ 6 };  // State vector dimension
+    static constexpr int n_m{ 4 };  // Measurement vector dimension
+    static constexpr int num_sigma_vec{ 2 * n_x + 1 };
+
+    UKF(float theta = 0.0f, float alpha = 1.0f, float beta = 2.0f, float kappa = 2.0f, float Ts = 0.01f,
+        float phi = 0.95f, float sigma_a = 0.2f);
+
+    void timeUpdate();
+ 
+    void measurementUpdate(BLA::Matrix<n_m>& y_k);
+ 
+    std::array<double, 2> getPosition() const;
+
+    void setGNSSNoise(float scale);
+    
+    void setHeading(float theta);
+
+    void simulateOutage(const int numMeasurements);
+
+private:
+    float lambda;   // Scaling parameter
+    float alpha;    // Sigma point spread
+    float beta;     // Distribution prior parameter
+    float kappa;    // Secondary scaling parameter
+    float k;        // Sigma point distance factor
+    float Ts;       // Sampling time
+    float phi;      // Acceleration smoothing factor
+    float sigma_a;  // Acceleration noise standard deviation
+    float theta;    // Compass heading
+
+    // Standard deviation measured for sensors
+    float sigma_gnss_x{ 2.461963 };
+    float sigma_gnss_y{ 1.420877 };
+    float sigma_imu_x{ 0.006728 };
+    float sigma_imu_y{ 0.006481 };
+
+    BLA::Matrix<4, 4> RotM; // Rotation matrix
+
+    // States
+    BLA::Matrix<n_x> x_hat;  // State estimate
+    BLA::Matrix<n_x, n_x> P; // State covariance matrix
+
+    // Noise
+    BLA::Matrix<n_x, n_x> Q; // Process noise covariance matrix
+    BLA::Matrix<n_m, n_m> R; // Measurement noise covariance matrix
+
+    // Sigma points
+    BLA::Matrix<n_x, num_sigma_vec> X_sigma; // Sigma points state matrix
+    BLA::Matrix<n_m, num_sigma_vec> Y_sigma; // Sigma points measurement matrix
+
+    BLA::Matrix<n_x, n_m> K;  // Kalman gain
+
+    // Weights
+    BLA::Matrix<num_sigma_vec> W_m; // Weights for mean
+    BLA::Matrix<num_sigma_vec> W_c; // Weights for covariance
+
+    BLA::Matrix<n_x, n_x> A; // State transition matrix
+    BLA::Matrix<n_m, n_x> H; // Measurement matrix
+
+    void computeWeights();
+
+    void createSigmaPoints();
+
+    void updateRotation();
+
+    void updateMeasurementNoise();
+
+};
--- deadReckoning/deadReckoning.ino
+++ deadReckoning/deadReckoning.ino
@@ -1,8 +1,8 @@
-#include "UKF.cpp" // NB. should be a cpp + .h file
-#include "WGS84toCartesian.hpp"
+#include "UKF.h"
 #include "IMUReader.h"
 #include "GNSSReader.h"
-#include <Wire.h> // Required for I2C 
+#include "WGS84toCartesian.hpp"
+#include <Wire.h>  // Required for I2C
 #include <BasicLinearAlgebra.h>
 #include <array>
 
@@ -12,50 +12,49 @@ IMUReader imu{ Wire, LSM6DS3_ADDRESS };
 constexpr float headingDegrees{ 180.0f };
 UKF ukf(headingDegrees);
 
-void setup()
-{
-    Serial.begin(115200);
-    while (!Serial); // Wait for the serial port to initialize
-    Wire.begin(); // Initialize I2C
-    if (!gnss.begin()) { Serial.println("Failed to initialize GNSS"); }
-    if (!imu.begin()) { Serial.println("Failed to initialize IMU"); } 
-    imu.setAccRate13Hz();
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);  // Wait for the serial port to initialize
+  Wire.begin();     // Initialize I2C
+  if (!gnss.begin()) { Serial.println("Failed to initialize GNSS"); }
+  if (!imu.begin()) { Serial.println("Failed to initialize IMU"); }
+  imu.setAccRate13Hz();
 }
 
 void loop() {
-    
-    static bool referenceInitialized{ false };
-    static std::array<double, 2> lastReference;
-    static std::array<double, 2> currentPosition;
-    static std::array<double ,2> cartesianPosition;
-    static BLA::Matrix<ukf.n_m> measurements;
-    
-    if (gnss.updateCoordinates() && imu.updateAccelerometer()) { // NB. GNSS receiver sample rate is 1 Hz
-        const auto& coords = gnss.getCoordinates();
 
-      if (!referenceInitialized) {
-        lastReference = { coords.latitude, coords.longitude };
-        referenceInitialized = true;
-        return;
-      }
+  static bool referenceInitialized{ false };
+  static std::array<double, 2> lastReference;
+  static std::array<double, 2> currentPosition;
+  static std::array<double, 2> cartesianPosition;
+  static BLA::Matrix<ukf.n_m> measurements;
 
-        currentPosition = { coords.latitude, coords.longitude };
-        cartesianPosition = wgs84::toCartesian(lastReference, currentPosition); // Convert to Cartesian in meters using the UTM projection
-        lastReference = currentPosition;
-        
-        const auto& accData = imu.getAccData();
-        measurements = { cartesianPosition[0], cartesianPosition[1], accData.accX, accData.accY };
-        
-        // Compute the positional state prediction using the UKF
-        ukf.timeUpdate(); 
-        ukf.measurementUpdate(measurements);
+  if (gnss.updateCoordinates() && imu.updateAccelerometer()) {  // NB. Default GNSS receiver sample rate is 1 Hz
+    const auto& coords = gnss.getCoordinates();
 
-        // Print the measurement and estimate for data acquisition using serial_reader.py
-        gnss.printToSerial(cartesianPosition, ukf.getPosition(), 10);
-
-        // Increase the noise covariance by 10x to after 50 measurements
-        // to simulate GNSS intereference 
-        ukf.simulateOutage(5);
+    if (!referenceInitialized) {
+      lastReference = { coords.latitude, coords.longitude };
+      referenceInitialized = true;
+      return;
     }
-}
 
+    currentPosition = { coords.latitude, coords.longitude };
+    // Convert to Cartesian position in meters using the UTM projection
+    cartesianPosition = wgs84::toCartesian(lastReference, currentPosition);
+    lastReference = currentPosition;
+
+    const auto& accData = imu.getAccData();
+    measurements = { static_cast<float>(cartesianPosition[0]), static_cast<float>(cartesianPosition[1]), accData.accX, accData.accY };
+
+    // Compute the positional state prediction using the UKF
+    ukf.timeUpdate();
+    ukf.measurementUpdate(measurements);
+
+    // Print the measurement and estimate for data acquisition using serial_reader.py
+    gnss.printToSerial(cartesianPosition, ukf.getPosition(), 10);
+
+    // Increase the noise covariance by 10x to after 50 measurements
+    // to simulate GNSS intereference
+    ukf.simulateOutage(50);
+  }
+}
